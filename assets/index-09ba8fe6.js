ea1a6b538ebe7cc38192dd41884a8d99e3437699
colorLei <18871174108@163.com>
2023-06-09 09:58:59 +0800

    112

--- assets/index-09ba8fe6.js
+++
@@ -1,8 +0,0 @@
-import{d,r as n,c as t,a as s,F as p,f as _,u,o as a,b as o,w as v,t as m,p as g,h}from"./index-f4f894f3.js";import{_ as y}from"./_plugin-vue_export-helper-c27b6911.js";const x="/assets/avater1-1700a59b.png",f="/assets/avater2-6ec13e97.png",w="/assets/avater3-385a256c.png",S=[{avater:x,title:"Employer Sponsored",describe:`We focus on ergonomics and
- meeting you where you work. `},{avater:f,title:"SUV Immigration",describe:`Just type what's on your mind
- and we'll get you there.`},{avater:w,title:"Skilled Migration",describe:`the quick fox jumps over the
- lazy dog`}],b=e=>(g("data-v-3852d076"),e=e(),h(),e),k={class:"wrap"},C=b(()=>s("br",null,null,-1)),I={class:"text-[14px] whitespace-pre-line my-10"},V=d({__name:"index",setup(e){return(B,L)=>{const i=n("v-list-item"),r=n("v-list");return a(),t("div",k,[s("div",null,[C,(a(!0),t(p,null,_(u(S),(c,l)=>(a(),t("div",{key:l},[o(r,{lines:"one",style:{background:"transparent"},class:"_aboutUsVlist"},{default:v(()=>[o(i,{title:"Carissa Sinclair",subtitle:"Leading Counsel","prepend-avatar":c.avater,style:{padding:"0"}},null,8,["prepend-avatar"]),s("p",I,m(`Carissa is the Client Services Manager at VisaPlace.She has extensive
- experience in a variety of industries, always with a focus on client care
- and account management. She is passionate about serving our
- immigration clients alongside our practitioners, and assuring her
- team sets a high standard for customer service. `),1)]),_:2},1024)]))),128))])])}}});const M=y(V,[["__scopeId","data-v-3852d076"]]);export{M as default};
---
+++ assets/index-fad1a4ab.js
@@ -0,0 +1,8 @@
+import{d,r as n,c as t,a as s,F as p,f as _,u,o as a,b as o,w as v,t as m,p as g,h}from"./index-c0d85e3f.js";import{_ as f}from"./_plugin-vue_export-helper-c27b6911.js";const y="/office-web/assets/avater1-1700a59b.png",w="/office-web/assets/avater2-6ec13e97.png",x="/office-web/assets/avater3-385a256c.png",b=[{avater:y,title:"Employer Sponsored",describe:`We focus on ergonomics and
+ meeting you where you work. `},{avater:w,title:"SUV Immigration",describe:`Just type what's on your mind
+ and we'll get you there.`},{avater:x,title:"Skilled Migration",describe:`the quick fox jumps over the
+ lazy dog`}],S=e=>(g("data-v-3852d076"),e=e(),h(),e),k={class:"wrap"},C=S(()=>s("br",null,null,-1)),I={class:"text-[14px] whitespace-pre-line my-10"},V=d({__name:"index",setup(e){return(B,L)=>{const i=n("v-list-item"),r=n("v-list");return a(),t("div",k,[s("div",null,[C,(a(!0),t(p,null,_(u(b),(c,l)=>(a(),t("div",{key:l},[o(r,{lines:"one",style:{background:"transparent"},class:"_aboutUsVlist"},{default:v(()=>[o(i,{title:"Carissa Sinclair",subtitle:"Leading Counsel","prepend-avatar":c.avater,style:{padding:"0"}},null,8,["prepend-avatar"]),s("p",I,m(`Carissa is the Client Services Manager at VisaPlace.She has extensive
+ experience in a variety of industries, always with a focus on client care
+ and account management. She is passionate about serving our
+ immigration clients alongside our practitioners, and assuring her
+ team sets a high standard for customer service. `),1)]),_:2},1024)]))),128))])])}}});const M=f(V,[["__scopeId","data-v-3852d076"]]);export{M as default};
